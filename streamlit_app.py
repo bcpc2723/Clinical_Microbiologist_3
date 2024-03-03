85441529d07fe58cccab1d3572e10920891139a1
bcpc2723 <156993514+bcpc2723@users.noreply.github.com>
2024-03-03 16:40:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,4 @@ if prompt := st.chat_input("What is up?"):
                 full_response += response.choices[0].delta.content
                 message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    st.session_state.messages.append({"role": "Clinical Biologist specified in Viral genome analysis, Elucidation of the pathogenesis and Population virus seroprevalence", "content": full_response})
