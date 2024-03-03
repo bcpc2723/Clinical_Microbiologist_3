2b9106c0605936d890ade8efe62f75edb66ac3f0
bcpc2723 <156993514+bcpc2723@users.noreply.github.com>
2024-03-03 15:12:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,8 @@ if prompt := st.chat_input("What is up?"):
             model="gpt-3.5-turbo",
             messages=[{"role": m["role"], "content": m["content"]}
                       for m in st.session_state.messages], stream=True):
-            full_response += response.choices[0].delta.content
-            message_placeholder.markdown(full_response + "▌")
+            if response.choices[0].delta.content is not None:
+                full_response += response.choices[0].delta.content
+                message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
