3af543a5fb7e6d17fc13770926e52ab81db8b1e4
bcpc2723 <156993514+bcpc2723@users.noreply.github.com>
2024-03-03 15:56:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import openai
 from openai import OpenAI
 import streamlit as st
 
-#ASSISTANT_ROLE = "Clinical Biologist specified in Viral genome analysis, Elucidation of the pathogenesis and Population virus seroprevalence"
-
 with st.sidebar:
     st.title('🥼💬 Clinical Biologist for mNGS analysis')
     if 'OPENAI_API_KEY' in st.secrets:
